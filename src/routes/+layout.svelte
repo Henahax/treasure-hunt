be5923bf29136f743a1071934c829af42026a976
Henahax <your_private_email@users.noreply.github.com>
2026-09-24 00:53:47 +0200

    menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,30 @@
 
 	import Menu from '$lib/components/Menu.svelte';
 
+	import { resolve } from '$app/paths';
+
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
 <div class="grid min-h-dvh grid-rows-[auto_minmax(0,1fr)_auto_auto]">
+	<nav aria-label="App-Menu" class="border-t border-neutral-700 bg-red-500 p-2 sm:bg-green-500">
+		<ul class="mx-auto flex max-w-md items-center justify-around gap-2">
+			<li>
+				<a href={resolve('/')} class="btn btn-ghost">
+					<i class="fa-solid fa-house"></i>
+					{translator.t('nav.home')}
+				</a>
+			</li>
+			<li>
+				<a href={resolve('/treasure-hunts')} class="btn btn-ghost">
+					<i class="fa-solid fa-map"></i>
+					{translator.t('nav.treasure-hunts')}
+				</a>
+			</li>
+		</ul>
+	</nav>
 	<header class="p-4">
 		<h1>{translator.t('title')}</h1>
 	</header>
